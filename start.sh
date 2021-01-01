4f18cfc82a5f18da52f9d0592d867d12d27b76fb
Filipe Pina <fopina@gmail.com>
2021-01-01 19:26:24 +0000

    use docker0

--- start.sh
+++ start.sh
@@ -4,13 +4,25 @@ cd $(dirname $0)
 
 function master() {
     multipass launch --name k3s-master --mem 1G --disk 5G bionic
-    multipass exec k3s-master -- sudo sh -c "curl -sfL https://get.k3s.io | sh -"
+    multipass exec k3s-master -- sh -c "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -"
+    multipass exec k3s-master -- sh -c 'sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"'
+    multipass exec k3s-master -- sudo apt-get install docker-ce docker-ce-cli containerd.io
+    multipass exec k3s-master -- sudo sh -c "curl -sfL https://get.k3s.io | sh -s --docker"
 }
 
 function worker() {
     NAME=${1:-worker}
     multipass launch --name k3s-$NAME --mem 1G --disk 5G bionic
-    multipass exec k3s-$NAME -- sudo sh -c "curl -sfL https://get.k3s.io | K3S_URL=\"https://${MASTER_IP}:6443\" K3S_TOKEN=\"${TOKEN}\" sh -"
+    multipass exec k3s-$NAME -- sh -c "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -"
+    multipass exec k3s-master -- sh -c 'sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"'
+    multipass exec k3s-master -- sudo apt-get install docker-ce docker-ce-cli containerd.io
+    multipass exec k3s-$NAME -- sudo sh -c "curl -sfL https://get.k3s.io | K3S_URL=\"https://${MASTER_IP}:6443\" K3S_TOKEN=\"${TOKEN}\" sh - --docker"
 }
 
 TOKEN=$(multipass exec k3s-master sudo cat /var/lib/rancher/k3s/server/node-token)
