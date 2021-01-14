abd7294df2ef96bb754191058c8f074b7b7462cd
Filipe Pina <fopina@gmail.com>
2021-01-14 00:04:30 +0000

    switching master: passed

--- switch_master.sh
+++ switch_master.sh
@@ -6,6 +6,7 @@ set -e
 
 # such as --docker
 K3S_ARGS="$@"
+MASTER=worker1
 
 function install_docker() {
     test "${K3S_ARGS#*docker}" == "$K3S_ARGS" && return 0
@@ -20,27 +21,25 @@ function install_docker() {
 
 function master() {
     NAME=${1:-master}
-    multipass launch --name k3s-$NAME --mem 1G --disk 5G bionic || true
     install_docker k3s-$NAME
     multipass exec k3s-$NAME -- sudo sh -c "curl -sfL https://get.k3s.io | sh -s -- ${K3S_ARGS}"
 }
 
 function worker() {
     NAME=${1:-worker}
-    multipass launch --name k3s-$NAME --mem 1G --disk 5G bionic || true
     install_docker k3s-$NAME
     multipass exec k3s-$NAME -- sudo sh -c "curl -sfL https://get.k3s.io | K3S_URL=\"https://${MASTER_IP}:6443\" K3S_TOKEN=\"${TOKEN}\" sh -s -- ${K3S_ARGS}"
 }
 
-master
+master ${MASTER}
 
-TOKEN=$(multipass exec k3s-master sudo cat /var/lib/rancher/k3s/server/node-token)
-MASTER_IP=$(multipass info k3s-master --format csv | cut -d ',' -f3 | tail -n1)
+TOKEN=$(multipass exec k3s-${MASTER} sudo cat /var/lib/rancher/k3s/server/node-token)
+MASTER_IP=$(multipass info k3s-${MASTER} --format csv | cut -d ',' -f3 | tail -n1)
 
-worker worker1
+worker master
 worker worker2
 
-multipass exec k3s-master -- sudo kubectl config view --raw | sed -e "s/server: .*/server: https:\/\/${MASTER_IP}:6443/g" > .kubeconfig
+multipass exec k3s-${MASTER} -- sudo kubectl config view --raw | sed -e "s/server: .*/server: https:\/\/${MASTER_IP}:6443/g" > .kubeconfig
 
 echo To use local kubectl run:
 echo export KUBECONFIG=$(pwd)/.kubeconfig
